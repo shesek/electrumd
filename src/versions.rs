6074cf283ca517469f4dae50d5d70b773d8f7977
Nadav Ivgi <nadav@shesek.info>
2024-04-05 19:19:46 +0300

    cargo fmt

--- src/versions.rs
+++ src/versions.rs
@@ -1,10 +1,6 @@
-pub const HAS_FEATURE: bool = cfg!(any(
-    feature = "4_1_5",
-));
+pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5",));
 
-#[cfg(not(any(
-    feature = "4_1_5",
-)))]
+#[cfg(not(any(feature = "4_1_5",)))]
 pub const VERSION: &str = "N/A";
 
 #[cfg(feature = "4_1_5")]

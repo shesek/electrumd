e59544fd20bcda219964b50dfbf6366acf644e55
Riccardo Casatta <riccardo@casatta.it>
2024-08-12 15:14:32 +0200

    add electrum 4.5.4

--- src/versions.rs
+++ src/versions.rs
@@ -1,7 +1,10 @@
-pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5",));
+pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5", feature = "4_5_4"));
 
-#[cfg(not(any(feature = "4_1_5",)))]
+#[cfg(not(any(feature = "4_1_5", feature = "4_5_4")))]
 pub const VERSION: &str = "N/A";
 
 #[cfg(feature = "4_1_5")]
 pub const VERSION: &str = "4.1.5";
+
+#[cfg(feature = "4_5_4")]
+pub const VERSION: &str = "4.5.4";

e22e864d1ce2ebbf02f1e3fee1ac846ab54bcafd
Nadav Ivgi <nadav@shesek.info>
2026-01-27 19:10:28 +0200

    Add Electrum v4.7.0

--- src/versions.rs
+++ src/versions.rs
@@ -1,6 +1,6 @@
-pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2"));
+pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0"));
 
-#[cfg(not(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2")))]
+#[cfg(not(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0")))]
 pub const VERSION: &str = "N/A";
 
 #[cfg(feature = "4_1_5")]
@@ -11,3 +11,6 @@ pub const VERSION: &str = "4.5.4";
 
 #[cfg(feature = "4_6_2")]
 pub const VERSION: &str = "4.6.2";
+
+#[cfg(feature = "4_7_0")]
+pub const VERSION: &str = "4.7.0";

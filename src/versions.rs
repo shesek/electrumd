8b4a286d4ae8b8f0e7842bfef25c3310e212b822
Nadav Ivgi <nadav@shesek.info>
2026-02-01 23:24:00 +0200

    Add 4.6.0

--- src/versions.rs
+++ src/versions.rs
@@ -1,10 +1,13 @@
-pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0"));
+pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5", feature = "4_6_0", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0"));
 
-#[cfg(not(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0")))]
+#[cfg(not(any(feature = "4_1_5", feature = "4_6_0", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0")))]
 pub const VERSION: &str = "N/A";
 
 #[cfg(feature = "4_1_5")]
 pub const VERSION: &str = "4.1.5";
+
+#[cfg(feature = "4_6_0")]
+pub const VERSION: &str = "4.6.0";
 
 #[cfg(feature = "4_5_4")]
 pub const VERSION: &str = "4.5.4";

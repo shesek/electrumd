13ce231350a00926032deb47b20bb3dc4a2df356
Nadav Ivgi <nadav@shesek.info>
2026-09-01 09:04:27 +0000

    Use platform-specific Electrum executable paths
    And remove the macOS symlink hack.

--- src/versions.rs
+++ src/versions.rs
@@ -1,6 +1,22 @@
-pub const HAS_FEATURE: bool = cfg!(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0"));
+pub const HAS_FEATURE: bool = cfg!(any(
+    feature = "4_1_5",
+    feature = "4_5_4",
+    feature = "4_6_2",
+    feature = "4_7_0"
+));
 
-#[cfg(not(any(feature = "4_1_5", feature = "4_5_4", feature = "4_6_2", feature = "4_7_0")))]
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+pub const EXE_REL_PATH: &str = "electrum.AppImage";
+
+#[cfg(target_os = "macos")]
+pub const EXE_REL_PATH: &str = "Electrum.app/Contents/MacOS/run_electrum";
+
+#[cfg(not(any(
+    feature = "4_1_5",
+    feature = "4_5_4",
+    feature = "4_6_2",
+    feature = "4_7_0"
+)))]
 pub const VERSION: &str = "N/A";
 
 #[cfg(feature = "4_1_5")]
